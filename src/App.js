cf08e1e46a69c3161428696db61fc74a216bca1b
patomation <patomation@gmail.com>
2019-12-15 21:47:50 +0700

    feature: double input update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, {useState} from "react";
 
 import { Button, Center, Panel, Gutter } from '@patomation/react-ui-components'
 import CssConverter from './components/CssConverter'
+import colorLuminance from './modules/colorLuminance.js'
+
+const darkGray = '#37323e'
 
 const App = () =>
 
 <div className='app'>
-  <Center>
-    <Panel className='panel'>
+  <Center maxWidth='800px'>
+    <Panel
+      className='panel'
+      style={{
+        background: colorLuminance(darkGray, -0.1)
+      }}>
       <CssConverter />
     </Panel>
   </Center>
--- src/components/CssConverter/index.js
+++ src/components/CssConverter/index.js
@@ -1,33 +1,64 @@
 import React, { useState } from 'react'
-import { Button, Center, Gutter } from '@patomation/react-ui-components'
-import convert from './convert.js';
+import { Button, Center, Gutter, Input, Grid } from '@patomation/react-ui-components/src'
+import convert from './convert.js'
+import colorLuminance from '../../modules/colorLuminance.js'
+
+const silver = '#bfbdc1',
+      gray = '#6d6a75',
+      darkGray = '#37323e',
+      lightOrange = '#deb841',
+      darkOrange = '#de9e36'
 
 const CssConverter = () => {
 
-  const [value, setValue] = useState()
+  const [input, setInput] = useState()
+  const [output, setOutput] = useState()
 
   return (
-    <div className='CssConverter'>
+    <div className='CssConverter' style={{color: darkOrange}}>
       <h1 style={{textAlign: 'center'}}>React Inline Style Converter</h1>
       <h3 style={{textAlign: 'center'}}>CSS to inline styles</h3>
       <Gutter/>
+      <Gutter/>
 
-      <textarea
-        rows="10"
-        cols="60"
-        value={value}
-        onChange={ e => setValue(e.target.value)}
-        style={{
-          width: '100%',
-
-        }}>
-      </textarea>
+      <Grid col={2} gap>
+        <Input
+          label='css'
+          type='textarea'
+          rows="6"
+          value={input}
+          onChange={ e => setInput(e.target.value)}
+          color={lightOrange}
+          background={colorLuminance(darkGray, -0.2)}
+          inputStyle={{
+            border: '1px solid #de9e36' //darkOrange
+          }}
+          />
+        <Input
+          label='inline css'
+          type='textarea'
+          rows="6"
+          value={output}
+          onChange={ e => setOutput(e.target.value)}
+          color={lightOrange}
+          background={colorLuminance(darkGray, -0.2)}
+          inputStyle={{
+            border: '1px solid #de9e36' //darkOrange
+          }}
+          />
+      </Grid>
       <Gutter />
 
-      <Button center title='convert' onClick={()=>{
-        console.log(value);
-        console.log((convert(value)));
-        setValue(convert(value))
+      <Button
+        disabled={!input}
+        center
+        title='convert'
+        color={darkGray}
+        background={darkOrange}
+        onClick={()=>{
+          console.log(input);
+          console.log((convert(input)));
+          setOutput(convert(input))
       }}/>
     </div>
   )
